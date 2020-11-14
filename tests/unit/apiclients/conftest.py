a1e227d93e56890d83feba49eeeec84171dddee3
SaiyajiiN <sangokanj@gmail.com>
2020-11-14 11:51:01 +0100

    Implemented tests for validators and normalizers

--- tests/unit/apiclients/conftest.py
+++ tests/unit/apiclients/conftest.py
@@ -4,6 +4,9 @@ import requests
 import tests.unit.apiclients.test_data as data
 
 from apps.apiclients.clients import OpenfoodfactsClient
+from apps.apiclients.validators import ProductValidator
+from apps.apiclients.normalizers import ProductNormalizer
+from tests.unit.apiclients.test_data import VALID_PRODUCT
 
 
 OPENFOODFACTS_SUCCESS_RESPONSE = {
@@ -23,14 +26,14 @@ OPENFOODFACTS_NOTHING_FOUND_RESPONSE = {
 
 @pytest.fixture
 def client():
-    """Fixture creating an instance of OpenfoodfactsClient for each test."""
+    """Fixture créant une instance de OpenfoodfactsClient pour chaque test."""
     client = OpenfoodfactsClient()
     yield client
 
 
 @pytest.fixture
 def mock_get(mocker):
-    """Fixture replacing requests.get function with an imitation."""
+    """Fixture remplacant la fonction requests.get par une imitation."""
 
     mock_requests_get = mocker.patch('requests.get')
     mock_requests_get.return_value.json.return_value = (
@@ -41,8 +44,8 @@ def mock_get(mocker):
 
 @pytest.fixture
 def mock_get_with_http_error(mocker):
-    """Fixture replacing requests.get function with an imitation
-    raising a request.HTTPError via raise_for_status."""
+    """Fixture remplacant la fonction requests.get par une imitation
+    levant une requests.HTTPError via raise_for_status."""
 
     mock_requests_get = mocker.patch('requests.get')
     mock_requests_get.return_value.json.return_value = (
@@ -56,8 +59,8 @@ def mock_get_with_http_error(mocker):
 
 @pytest.fixture
 def mock_get_with_connection_error(mocker):
-    """Fixture replacing requests.get function with an imitation
-    raising a requests.ConnectionError."""
+    """Fixture remplacant la fonction requests.get par une imitation
+    levant une requests.ConnectionError."""
 
     mock_requests_get = mocker.patch('requests.get')
     mock_requests_get.side_effect = requests.ConnectionError(
@@ -68,8 +71,8 @@ def mock_get_with_connection_error(mocker):
 
 @pytest.fixture
 def mock_get_with_no_result(mocker):
-    """Fixture replacing requests.get function with an imitation
-    simulating an unsuccessful call to the API."""
+    """Fixture remplacant la fonction requests.get par une imitation
+    simulant un appel sans résultat à l'API."""
 
     mock_requests_get = mocker.patch('requests.get')
     mock_requests_get.return_value.json.return_value = (
@@ -79,8 +82,18 @@ def mock_get_with_no_result(mocker):
 
 
 @pytest.fixture
+def validator():
+    yield ProductValidator()
+
+
+@pytest.fixture
 def valid_products():
     yield data.get_valid_products()
+
+
+@pytest.fixture
+def valid_product():
+    return VALID_PRODUCT
 
 
 @pytest.fixture
@@ -91,3 +104,8 @@ def invalid_products():
 @pytest.fixture
 def invalid_with_one_valid_product():
     yield data.get_invalid_with_one_valid_product()
+
+
+@pytest.fixture
+def normalizer():
+    return ProductNormalizer()
